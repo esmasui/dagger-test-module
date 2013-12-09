590734e5c99e659ca4bf9bbe2556d85e59688c16
Sosuke Masui <esmasui@gmail.com>
2013-12-10 02:21:10 +0900

    Add example.

---
+++ example/src/main/java/coffee/CoffeeMaker.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package coffee;
+
+import dagger.Lazy;
+import javax.inject.Inject;
+
+class CoffeeMaker {
+  @Inject Lazy<Heater> heater; // Don't want to create a possibly costly heater until we need it.
+  @Inject Pump pump;
+
+  public void brew() {
+    heater.get().on();
+    pump.pump();
+    System.out.println(" [_]P coffee! [_]P ");
+    heater.get().off();
+  }
+}
